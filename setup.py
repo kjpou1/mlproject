e2982cd779363bc712d888f21bbe7d5bb5fcc9ce
Kenneth Pouncey <kjpou@pt.lu>
2025-01-21 08:09:07 +0100

    update comments

--- setup.py
+++ setup.py
@@ -23,9 +23,15 @@ def get_requirements(file_path: str) -> List[str]:
             ]
         return requirements
     except FileNotFoundError:
-        print(f"Error: {file_path} not found.")
         warnings.warn(
-            f"Warning: {file_path} not found. No dependencies were installed from this file."
+            f"Warning: {file_path} not found. Proceeding without additional dependencies.",
+            stacklevel=2,
+        )
+        return []
+    except UnicodeDecodeError as e:
+        warnings.warn(
+            f"Warning: Encoding issue while reading {file_path}: {str(e)}. Proceeding without additional dependencies.",
+            stacklevel=2,
         )
         return []
 
@@ -37,4 +43,10 @@ setup(
     author_email="wasssssuuuuupppp@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements("requirements.txt"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],  # Metadata for PyPI
+    python_requires=">=3.7",  # Minimum Python version
 )
